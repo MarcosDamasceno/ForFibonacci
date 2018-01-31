9540bfc6ae9482367fd8928a7c53817702034cd7
Marcos Damasceno e Souza <35048452+MarcosDamasceno@users.noreply.github.com>
2018-01-31 20:02:01 -0200

    Commit inicial do projeto

---
+++ src/forfibonacci/ForFibonacci.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forfibonacci;
+
+/**
+ *
+ * @author meumd
+ */
+public class ForFibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int fibonacci = 1;
+        int numAnterior = 0;
+        System.out.print("A série Fibonacci compõe-se dos números: " + numAnterior + ", " + fibonacci + ", ");
+        for (int i = 0; fibonacci <= 100; i++) {
+            int resultado = numAnterior + fibonacci;
+            numAnterior = fibonacci;
+            fibonacci = resultado;
+
+            System.out.print(resultado + ", ");
+
+        }
+        System.out.print("etc");
+        System.out.println("");
+    }
+
+}
